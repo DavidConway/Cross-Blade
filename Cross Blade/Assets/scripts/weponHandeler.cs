ad078f7bc31b9b1b39ad07033d9d407dffdbaade
DavidConway <dconway099@gmail.com>
2020-11-18 16:16:17 +0000

    alpha 1.0.1 compleat + bug fix
    all featchers for alpha 1.0.1 have been implemented
    sucseful build for quest 2
    out of bounds bug fixed

---
+++ Cross Blade/Assets/scripts/weponHandeler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weponHandeler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject myCol = collision.contacts[0].thisCollider.gameObject;
+        Debug.Log(myCol.tag);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject myCol = this.gameObject;
+        Debug.Log(myCol.tag);
+    }
+}
